e25f57550fe4c620536eddc0b1b4d3303b89c3b6
katrinnp <147139291+katrinnp@users.noreply.github.com>
2026-02-11 22:25:08 +0200

    Add User-Task relationship (#9)

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String, DateTime, Boolean
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Boolean
 from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
 from app.core.database import Base
 
 class Task(Base):
@@ -10,4 +11,9 @@ class Task(Base):
     completed = Column(Boolean, default = False)
     created_at = Column(DateTime(timezone = True), server_default = func.now())
     updated_at = Column(DateTime(timezone = True), onupdate = func.now())
+    user_id = Column(Integer, 
+                     ForeignKey("users.id"), #Links each task to a specific user
+                     nullable = False)
+    owner = relationship("User", back_populates = "tasks") #ORM relationship back to the User model
+
     
