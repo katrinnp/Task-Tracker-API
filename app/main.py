e689e9584065f9f2fc64360c7db9c7d9073634a2
katrinnp <147139291+katrinnp@users.noreply.github.com>
2026-03-12 22:35:35 +0200

    Fix CORS configuration (#29)

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI #FastAPI web framework
 from fastapi.middleware.cors import CORSMiddleware #Allows requests from frontend
-from app.api.v1 import tasks, auth
 
 app = FastAPI(title="Task Tracker API") #Create FastAPI instance
 
@@ -13,11 +12,13 @@ origins = [
 #Enable CORS so React can make requests
 app.add_middleware(
     CORSMiddleware, #Built-in FastAPI CORS middleware
-    allow_origins=origins, #Allow requests from frontend
+    allow_origins=["*"], #Allow requests from frontend
     allow_credentials=True, #Allow auth headers to be sent
     allow_methods=["*"], #Allow GET, POST, PUT, PATCH, DELETE
     allow_headers=["*"] #Allow all request headers
 )
+
+from app.api.v1 import tasks, auth
 
 from app.models.task import Task #Task model (database table)
 from app.models.user import User #User model (database table)
@@ -26,6 +27,7 @@ from app.api.v1.tasks import router as tasks_router #Router with all task endpoi
 from app.api.v1.users import router as users_router #Router with all user endpoints
 
 from app.core.database import Base
+
 Base.metadata.create_all(bind = engine)
 
 app.include_router(tasks_router, prefix = "/tasks", tags = ["Tasks"]) #Include tasks router under /tasks path
